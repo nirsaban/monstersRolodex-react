a4024dcaf114c80abb80f2769d03fc4a5112d2ef
nirsa <nirsa11@gmail.com>
2020-05-07 15:58:21 +0300

    Updates

---
+++ precache-manifest.58107993f3f368378ce662012e826642.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fecfe1999d03bf6daae10754799534ee",
+    "url": "/monstersRolodex-react/index.html"
+  },
+  {
+    "revision": "d1b6f5f775024b3e9f44",
+    "url": "/monstersRolodex-react/static/css/main.7a0ab727.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monstersRolodex-react/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstersRolodex-react/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1b6f5f775024b3e9f44",
+    "url": "/monstersRolodex-react/static/js/main.8a4ec749.chunk.js"
+  },
+  {
+    "revision": "228e8fb234d4eac381f1",
+    "url": "/monstersRolodex-react/static/js/runtime-main.063ae5ce.js"
+  }
+]);
